11914283f37e06b6a4fcbc310d1572a24f60b176
Xavi122323 <xavier.ramos@udla.edu.ec>
2024-10-06 15:26:06 -0500

    interface handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,49 @@
 import React, { useState } from 'react';
-//import './App.css'
 import SideNavbar from './components/SideNavbar';
 import ClientList from './components/ClientList';
 import TopBar from './components/TopBar';
 import SearchBar from './components/SearchBar';
 import ClientDetails from './components/ClientDetails';
+import ClientInfo from './components/ClientInfo';
 import { Box } from '@mui/material';
 
 function App() {
   const [searchQuery, setSearchQuery] = useState('');
   const [category, setCategory] = useState('');
   const [selectedClient, setSelectedClient] = useState(null);
+  const [viewMode, setViewMode] = useState('recommendations');
 
   const handleCategoryChange = (selectedCategory) => {
     setCategory(selectedCategory);
     setSelectedClient(null);
   };
-
   const handleClientClick = (client) => {
     setSelectedClient(client);
   };
-
   const handleBackToList = () => {
     setSelectedClient(null);
+  };
+  const toggleViewMode = () => {
+    setViewMode((prevMode) => (prevMode === 'recommendations' ? 'info' : 'recommendations'));
   };
 
   return (
     <Box display="flex">
       <SideNavbar handleCategoryChange={handleCategoryChange} />
       <Box flexGrow={1} display="flex" flexDirection="column">
-        <TopBar onBackClick={handleBackToList} />
+        <TopBar
+          onBackClick={handleBackToList}
+          viewMode={viewMode}
+          toggleViewMode={toggleViewMode}
+        />
         <Box padding="20px">
           <SearchBar handleSearchChange={setSearchQuery} />
-          
           {selectedClient ? (
-            <ClientDetails client={selectedClient} />
+            viewMode === 'info' ? (
+              <ClientInfo client={selectedClient} />
+            ) : (
+              <ClientDetails client={selectedClient} />
+            )
           ) : (
             <ClientList
               searchQuery={searchQuery}
@@ -48,4 +57,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
