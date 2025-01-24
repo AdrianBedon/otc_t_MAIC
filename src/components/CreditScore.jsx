804ac8f561c5f9c576204196ca4fc312ef9502ec
Adrián Bedón <arbedon2002@gmail.com>
2025-01-23 19:03:05 -0500

    Version 4.0.6

--- src/components/CreditScore.jsx
+++ src/components/CreditScore.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
-import { Box, Typography } from '@mui/material';
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import PropTypes from "prop-types";
 
 const CreditScore = ({ score }) => {
   return (
     <Box mt={4} textAlign="center">
       <Typography variant="h6">Calificación crediticia</Typography>
-      <Typography variant="h1" style={{ fontSize: '80px', fontWeight: 'bold' }}>
+      <Typography variant="h1" style={{ fontSize: "80px", fontWeight: "bold" }}>
         {score}
       </Typography>
     </Box>
   );
+};
+
+CreditScore.propTypes = {
+  score: PropTypes.number,
 };
 
 export default CreditScore;
